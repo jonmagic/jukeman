c21e5fff44726cded04b0341a8acba05fb5ef664
Daniel Parker <dcparker@gmail.com>
2009-07-02 10:18:52 -0400

    Changed Journal records to inspected values.

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -6,12 +6,12 @@ class Journal < ActiveRecord::Base
 
     # SONGS
     def add_song(url, uuid)
-      Journal.record("Song.download(\"#{url}\", \"#{uuid}\")")
+      Journal.record("Song.download(#{url.inspect}, #{uuid.inspect})")
     end
     
     def remove_song(uuid)
       if Song.remove(uuid)
-        Journal.record("Song.remove(\"#{uuid}\")")
+        Journal.record("Song.remove(#{uuid.inspect})")
       else
         nil
       end
@@ -21,7 +21,7 @@ class Journal < ActiveRecord::Base
     # PLAYLISTS
     def new_playlist(name)
       if @playlist = Playlist.create(:name => name)
-        Journal.record("Playlist.create(\"#{name}\")")
+        Journal.record("Playlist.create(#{name.inspect})")
         return @playlist
       else
         nil
@@ -30,7 +30,7 @@ class Journal < ActiveRecord::Base
 
     def rename_playlist(old_name, new_name)
       if Playlist.rename(old_name, new_name)
-        Journal.record("Playlist.rename(\"#{old_name}\", \"#{new_name}\")")
+        Journal.record("Playlist.rename(#{old_name.inspect}, #{new_name.inspect})")
       else
         nil
       end
@@ -38,7 +38,7 @@ class Journal < ActiveRecord::Base
 
     def remove_playlist(playlist_name)
       if Playlist.remove(playlist_name)
-        Journal.record("Playlist.remove(\"#{playlist_name}\")")
+        Journal.record("Playlist.remove(#{playlist_name.inspect})")
       else
         nil
       end
@@ -46,8 +46,8 @@ class Journal < ActiveRecord::Base
 
     def add_song_to_playlist_and_reorder(playlist_name, song_uuid)
       if Playlist.add_song(playlist_name, song_uuid) && Playlist.reorder_items(playlist_name)
-        Journal.record("Playlist.add_song(\"#{playlist_name}\", \"#{song_uuid}\")")
-        Journal.record("Playlist.reorder_items(\"#{playlist_name}\")")
+        Journal.record("Playlist.add_song(#{playlist_name.inspect}, #{song_uuid.inspect})")
+        Journal.record("Playlist.reorder_items(#{playlist_name.inspect})")
       else
         nil
       end
@@ -55,7 +55,7 @@ class Journal < ActiveRecord::Base
     
     def reorder_playlist(playlist_name, ordinals)
       if Item.ordinal_shift(playlist_name, ordinals)
-        Journal.record("Item.ordinal_shift(\"#{playlist_name}\", \"#{ordinals}\")")
+        Journal.record("Item.ordinal_shift(#{playlist_name.inspect}, #{ordinals.inspect})")
       else
         nil
       end
@@ -65,8 +65,8 @@ class Journal < ActiveRecord::Base
     # ITEMS
     def remove_item_and_reorder_playlist(playlist_name, item_ordinal)
       if Item.remove(playlist_name, item_ordinal) && Playlist.reorder_items(playlist_name)
-        Journal.record("Item.remove(\"#{playlist_name}\", \"#{item_ordinal}\")")
-        Journal.record("Playlist.reorder_items(\"#{playlist_name}\")")
+        Journal.record("Item.remove(#{playlist_name.inspect}, #{item_ordinal.inspect})")
+        Journal.record("Playlist.reorder_items(#{playlist_name.inspect})")
       else
         nil
       end
