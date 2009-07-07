a29fcaca0a3df31d1dd82eb1f46cf97ad45a2e80
Jonathan Hoyt <jonmagic@Nikita.local>
2009-07-07 15:44:37 -0400

    working on making the apply to amarok stuff work

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -6,7 +6,13 @@ class Playlist < ActiveRecord::Base
   has_many :songs, :through => :items
   
   def apply_to_amarok
-    # run amarok apply method here
+    Amarok::Engine.stop
+    username = Dir.pwd.split('/')[2]
+    Amarok::Playlist.clearPlaylist
+    p.items.reverse.each do |item|
+      Amarok::Playlist.addMedia("/home/"+username+"/apps/jukeman/public/system/songs/"+item.song.id.to_s+"/original/"+item.song.song_file_name)
+    end
+    Amarok::Engine.play
   end
   
 
