d5f5b90c0ff4fb5af1c7416de9270ced264d27fe
Jonathan Hoyt <jonmagic@Sarah.local>
2010-02-26 13:26:36 -0500

    trying to get the right mix of frozen/unfrozen gems

--- vendor/gems/rack-gridfs-0.2.0/example/gridfs_server.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-
-require File.join(File.dirname(__FILE__), '..', 'lib', 'rack', 'gridfs')
-use Rack::GridFS, :hostname => 'localhost', :port => 27017, :database => 'test', :prefix => 'gridfs'
-
-get /.*/ do
-  "Whatchya talking about?"
-end
